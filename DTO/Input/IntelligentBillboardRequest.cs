5cb7863668b8146ff011c3e3ec46fb542370f969
Miriam <miriamantona@gmail.com>
2023-09-30 18:48:28 +0200

    Validations in DTO.

--- DTO/Input/IntelligentBillboardRequest.cs
+++ DTO/Input/IntelligentBillboardRequest.cs
@@ -1,13 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AppspaceChallenge.API.DTO.Input
 {
-  public class IntelligentBillboardRequest
+  public class IntelligentBillboardRequest: IValidatableObject
   {
-    //Add validation filters!
+    [Required(ErrorMessage = "The 'From' date is required.")]
     public DateTime From { get; set; }
+
+    [Required(ErrorMessage = "The 'To' date is required.")]
     public DateTime To { get; set; }
+
+    [Required(ErrorMessage = "The number of screens in big rooms is required.")]
     public int ScreensInBigRooms { get; set; }
+
+    [Required(ErrorMessage = "The number of screens in small rooms is required.")]
     public int ScreensInSmallRooms { get; set; }
+
     public bool IncludeSuccessfulMoviesInCity { get; set; }
+
     public int? CityId { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+      if (To < From)
+      {
+        yield return new ValidationResult(
+            errorMessage: "'To' date  must be greater than 'From' date",
+            memberNames: new[] { "To" }
+       );
+      }
+    }
   }
 }
